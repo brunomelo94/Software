fac75a67dea85c6659f08c098343fd26b2f3e847
Bruno Caetano Masculino de Melo <brunocaetano94@hotmail.com>
2018-05-24 17:02:57 -0300

    Deleted all.

--- pyserial_test1.py
+++
@@ -1,8 +0,0 @@
-import serial
-print("Antes")
-ser = serial.Serial('COM6',timeout=5)  # open serial port
-print("Depois")
-print(ser.name)         # check which port was really used
-ser.write(b'hello')     # write a string
-#ser.close()             # close port
-
